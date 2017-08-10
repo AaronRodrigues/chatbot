14467fc61b5d9b8d0526c68ab237a145463255c7
AaronRodrigues <aaronrod@gmail.com>
2017-08-10 21:41:19 +0100

    Twitch bot initializes with a logger -The logger is set to nil -A new logger is created when Twitch is initialized -requires logger library -requires the socket library for chat applications

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,4 +1,14 @@
+require 'socket'
+require 'logger'
+
 class Twitch
 
+attr_reader :logger, :running, :socket
+
+  def initialize(logger = nil)
+    @logger  = logger || Logger.new(STDOUT)
+    @running = false
+    @socket  = nil
+  end
 
 end
--- spec/bot_spec.rb
+++ spec/bot_spec.rb
@@ -1,11 +1,14 @@
 require 'bot'
 
 describe Twitch do
-	let(:bot) {described_class.new()}
+	let(:bot) {described_class.new(logger = nil)}
   
   describe 'initializes a bot' do
   	it 'exists' do
   		expect(bot).to be_truthy
-  	end  	
+  	end
+  	it 'initializes with a new logger' do
+  		expect(bot.logger).to be_kind_of(Logger)
+  	end	
   end
 end
